611c7387a9dc8dbcc5a01bb1e7872bae6e83b064
Andre Pereira <andre-luis.pereira@serpro.gov.br>
2025-09-21 16:40:12 -0300

    Adiciona cores ao terminal para melhor visualização das etapas no script build_n_push.sh

--- build_n_push.sh
+++ build_n_push.sh
@@ -2,6 +2,12 @@
 # Interrompe o script se qualquer comando falhar
 set -e
 
+# --- Cores para o terminal ---
+# Define códigos de cores para usar com 'echo -e'
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color (reseta a cor para o padrão)
+
 # --- Variáveis ---
 # Altere estas variáveis se quiser usar um nome de imagem ou usuário diferente
 DOCKERHUB_USERNAME="andrepereira21"
@@ -11,19 +17,25 @@ IMAGE_TAG="latest"
 FULL_IMAGE_NAME="${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${IMAGE_TAG}"
 
 # --- Execução ---
-echo "=> Fazendo login no Docker Hub..."
+#echo "=> Fazendo login no Docker Hub..."
 #docker login #Login não necessário com Docker Desktop instalado
 
-echo "=> Removendo projeto antigo no Kubernetes (se existir)..."
+echo
+echo -e "${YELLOW}=> Removendo projeto antigo no Kubernetes (se existir)..."
 kubectl delete all --all -n video-downloader
+echo
 
-echo "=> Atualizando o repositório local..."
+echo -e "${YELLOW}=> Atualizando o repositório local..."
 git pull origin main
+echo
 
-echo "=> Construindo a imagem Docker: ${FULL_IMAGE_NAME}"
+echo -e "${YELLOW}=> Construindo a imagem Docker: ${FULL_IMAGE_NAME}"
 sudo docker build -t "${FULL_IMAGE_NAME}" .
+echo
 
-echo "=> Publicando a imagem no Docker Hub..."
+
+echo -e "${YELLOW}=> Publicando a imagem no Docker Hub..."
 sudo docker push "${FULL_IMAGE_NAME}"
+echo
 
-echo "✅ Imagem construída e publicada com sucesso!"
+echo -e "${GREEN}✅ Imagem construída e publicada com sucesso!"
