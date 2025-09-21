5fcc1decc3ab4fa05431d8ff332e6abb181d53c4
Andre Pereira <andre-luis.pereira@serpro.gov.br>
2025-09-21 15:50:16 -0300

    Adiciona script para construção e publicação de imagem Docker no Docker Hub.

---
+++ app/build_n_push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Interrompe o script se qualquer comando falhar
+set -e
+
+# --- Variáveis ---
+# Altere estas variáveis se quiser usar um nome de imagem ou usuário diferente
+DOCKERHUB_USERNAME="andrepereira21"
+IMAGE_NAME="downloader"
+IMAGE_TAG="latest"
+
+FULL_IMAGE_NAME="${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${IMAGE_TAG}"
+
+# --- Execução ---
+echo "=> Fazendo login no Docker Hub..."
+#docker login #Login não necessário com Docker Desktop instalado
+
+echo "=> Construindo a imagem Docker: ${FULL_IMAGE_NAME}"
+docker build -t "${FULL_IMAGE_NAME}" .
+
+echo "=> Publicando a imagem no Docker Hub..."
+docker push "${FULL_IMAGE_NAME}"
+
+echo "✅ Imagem construída e publicada com sucesso!"
